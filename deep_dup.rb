86e80ace899c134e68b59ef26d87b347b6f55f13
Diego Novaes <dsnovaes1@gmail.com>
2022-10-20 21:58:06 -0700

    (almost) one liner deep dup

--- deep_dup.rb
+++ deep_dup.rb
@@ -1,7 +1,12 @@
 def deep_dup(arr)
     result = []
-    arr.each do |el|
-        ele.instance_of?(Array) ? result << deep_dup(el) : result << el
-    end
+    arr.each { |el| el.instance_of?(Array) ? result << deep_dup(el) : result << el }
     result
 end
+
+arrrr = [
+    ["nuts", "bolts", "washers"],
+    ["capacitors", "resistors", "inductors"]
+  ]
+
+p deep_dup(arrrr)
